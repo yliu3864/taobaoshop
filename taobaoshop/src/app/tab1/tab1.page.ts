9eda9ea3f758f84550e76bca4bdf08043120d7fd
Yan Liu <yliu3864@uwo.ca>
2021-03-07 20:16:35 -0500

    20 done

--- taobaoshop/src/app/tab1/tab1.page.ts
+++ taobaoshop/src/app/tab1/tab1.page.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-tab1',
@@ -7,6 +7,42 @@ import { Component } from '@angular/core';
 })
 export class Tab1Page {
 
-  constructor() {}
+  @ViewChild('slide1') slide1;
 
+  public slidesOpts={
+    speed : 400,
+    autoplay:{
+      delay: 2000,
+    },
+    loop: true
+  }
+  public slidesList:any[]=[];
+
+  public hotList:any[]=[];
+
+  public hotListWidth:any;
+
+  constructor() {
+
+    for(var i=1;i<=3;i++){
+
+      this.slidesList.push({
+        pic:'assets/slide0'+i+'.png',
+        url:'',
+      })
+    }
+
+    for(var i=1;i<=9;i++){
+
+      this.hotList.push({
+        pic:'assets/0'+i+'.jpg',
+        title:'第'+i+'个',
+      })
+    }
+
+    this.hotListWidth = this.hotList.length*9+'rem';
+  }
+  slideEnd(){
+    this.slide1.startAutoplay()
+  }
 }
