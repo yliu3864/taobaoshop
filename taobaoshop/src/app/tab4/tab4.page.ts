441d8a1c97f94ec5336be8eedadecf6afa94bf6e
Yan Liu <yliu3864@uwo.ca>
2021-03-14 23:58:06 -0400

    22 done

--- taobaoshop/src/app/tab4/tab4.page.ts
+++ taobaoshop/src/app/tab4/tab4.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
+
 @Component({
   selector: 'app-tab4',
   templateUrl: './tab4.page.html',
@@ -10,6 +11,26 @@ export class Tab4Page implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    setTimeout(function(){
+       document.getElementById("1").style.visibility = "visible"; }, 1000);
+
+    setTimeout(function(){
+      document.getElementById("1").style.visibility = "hidden";
+      document.getElementById("2").style.visibility = "visible";
+    }, 2000);
+
+    setTimeout(function(){
+      document.getElementById("3").style.visibility = "visible";
+      document.getElementById("1").style.visibility = "hidden";
+      document.getElementById("2").style.visibility = "hidden";}, 3000);
+
+    setTimeout(function(){
+      document.getElementById("1").style.visibility = "visible";
+      document.getElementById("2").style.visibility = "visible";
+      document.getElementById("3").style.visibility = "visible";
+
+     }, 4000);
+
   }
 
 }
