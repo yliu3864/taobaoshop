10fab3341bcf4b30c503a619cc0341c5768d7d9d
Yan Liu <yliu3864@uwo.ca>
2021-03-21 21:01:01 -0400

    23 done

--- taobaoshop/src/app/tab4/tab4.page.ts
+++ taobaoshop/src/app/tab4/tab4.page.ts
@@ -11,26 +11,6 @@ export class Tab4Page implements OnInit {
   constructor() { }
 
   ngOnInit() {
-    setTimeout(function(){
-       document.getElementById("1").style.visibility = "visible"; }, 1000);
-
-    setTimeout(function(){
-      document.getElementById("1").style.visibility = "hidden";
-      document.getElementById("2").style.visibility = "visible";
-    }, 2000);
-
-    setTimeout(function(){
-      document.getElementById("3").style.visibility = "visible";
-      document.getElementById("1").style.visibility = "hidden";
-      document.getElementById("2").style.visibility = "hidden";}, 3000);
-
-    setTimeout(function(){
-      document.getElementById("1").style.visibility = "visible";
-      document.getElementById("2").style.visibility = "visible";
-      document.getElementById("3").style.visibility = "visible";
-
-     }, 4000);
-
   }
 
 }
