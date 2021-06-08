7cad413e865540e035b878be95e01a3a609d0b1d
Yan Liu <yliu3864@uwo.ca>
2021-06-07 20:06:58 -0400

    33 done

--- taobaoshop/src/app/search/search.page.ts
+++ taobaoshop/src/app/search/search.page.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { CommonService } from '../services/common.service';
+import { IonContent } from '@ionic/angular';
 
 @Component({
   selector: 'app-search',
@@ -7,23 +9,98 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SearchPage implements OnInit {
 
-  flag=true;
-  productList=[];
+  @ViewChild(IonContent) content:IonContent;
+  flag = true;
+  productList = [];
+  keywords;
+  page = 1;
+  config: any = {};
+  hasInfinitData = true;
+  subHeaderList:any[]=[];
+  subHeaderSelected:any=1;
+  sort;
 
-  constructor() {
-    for(var i=1;i<=9;i++){
-      this.productList.push({
-        pic:'assets/0'+i+'.jpg',
-        title:'第'+i+'个',
-        price:i*22,
-      })
-    }
-   }
+  constructor(private common: CommonService, ) {
+    this.config = this.common.config;
+    this.subHeaderList=[
+      {
+        id:1,
+        title:"综合",
+        fields:"all",
+        sort:-1
+      },
+      {
+        id:2,
+        title:"销量",
+        fields:"salecount",
+        sort:-1
+      },
+      {
+        id:3,
+        title:"价格",
+        fields:"price",
+        sort:-1
+      },
+    ]
+  }
 
   ngOnInit() {
   }
 
-  doSearch(){
+  doSearch() {
     this.flag = false;
+    this.hasInfinitData = true;
+    this.page = 1;
+    this.subHeaderSelected=1;
+    this.content.scrollToTop(0);
+
+    var api = 'api/plist?search=' + this.keywords + "&page=" + this.page;
+
+
+    this.common.ajaxGet(api).then((res: any) => {
+
+      this.productList = res.result;
+      this.page++;
+
+    })
+  }
+
+  getProductList(event){
+
+
+    if(this.sort){
+      var api = 'api/plist?cid='+ this.keywords+"&page="+this.page+'&sort='+this.sort;
+    }else{
+      var api = 'api/plist?search=' + this.keywords + "&page=" + this.page;
+    }
+
+
+
+
+    this.common.ajaxGet(api).then((res:any)=>{
+      console.log(res)
+      this.productList=this.productList.concat(res.result);
+      this.page++;
+      event?event.target.complete():'';
+
+      if(res.result.length<10){
+        this.hasInfinitData = false;
+      }
+    })
+  }
+
+  subHeaderChange(id){
+    this.subHeaderSelected=id;
+
+
+
+    this.sort=this.subHeaderList[id-1].fields+'_'+this.subHeaderList[id-1].sort;
+    this.page=1;
+    this.productList=[];
+    this.hasInfinitData=true;
+    this.subHeaderList[id-1].sort=this.subHeaderList[id-1].sort*-1;
+    this.content.scrollToTop(0);
+
+    this.getProductList(null);
   }
 }
